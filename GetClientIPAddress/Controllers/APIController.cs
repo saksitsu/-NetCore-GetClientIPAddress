0d218459e6035abcca25b709ecb810b98c98c3c5
saksitsu <saksit.su2649@hotmail.com>
2019-05-05 15:39:42 +0700

    Start new project

---
+++ GetClientIPAddress/Controllers/APIController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GetClientIPAddress.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class APIController : Controller
+    {
+        private readonly IHttpContextAccessor httpContextAccessor;
+        public APIController(IHttpContextAccessor httpContextAccessor)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        [ActionName("GET_IPADDRESS")]
+        [HttpGet]
+        public JsonResult GET_IPADDRESS()
+        {
+            string str = "";
+
+            str = this.httpContextAccessor.HttpContext.Connection.RemoteIpAddress.ToString();
+
+            return Json(str);
+        }
+    }
+}
---
+++ GetClientIPAddress/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GetClientIPAddress.Models;
+using System.Net;
+using Microsoft.AspNetCore.Http;
+
+namespace GetClientIPAddress.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public HomeController(IHttpContextAccessor httpContextAccessor)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["ipaddress"] = this.httpContextAccessor.HttpContext.Connection.RemoteIpAddress.ToString();
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
